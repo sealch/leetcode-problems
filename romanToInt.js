3896dd07ad5d4bb13b7cf6b0aeecef300c1048b8
sealch <thesermanarm@gmail.com>
2018-03-30 13:26:09 +0300

    solved roman to integer

---
+++ romanToInt.js
@@ -0,0 +1,37 @@
+function romanToInt(s) {
+	var num = char_to_int(s.charAt(0));
+	var pre, curr;
+
+	for (var i = 1; i < s.length; i++) {
+		num1 = char_to_int(s.charAt(i));
+		num2 = char_to_int(s.charAt(i - 1));
+		if (num1 <= num2) {
+			num += num1;
+		} else {
+			num = num - num2 * 2 + num1;
+		}
+	}
+
+	return num;
+}
+
+function char_to_int(c) {
+	switch (c) {
+		case 'I':
+			return 1;
+		case 'V':
+			return 5;
+		case 'X':
+			return 10;
+		case 'L':
+			return 50;
+		case 'C':
+			return 100;
+		case 'D':
+			return 500;
+		case 'M':
+			return 1000;
+		default:
+			return -1;
+	}
+}
