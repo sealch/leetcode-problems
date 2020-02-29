8bbb8dac78e7a489d3f2d1240bdb5243a744cd28
sealch <thesermanarm@gmail.com>
2020-02-29 17:05:13 +0200

    update 1342. Number of Steps to Reduce a Number to Zero

--- 1342.js
+++ 1342.js
@@ -2,16 +2,11 @@
  * @param {number} num
  * @return {number}
  */
-let numberOfSteps  = (num) => {
-    let counter = 0;
+let numberOfSteps = (num) => {
+    let count = 0;
     while (num > 0) {
-        if (num % 2 === 0) {
-            num /= 2;
-            counter++;
-        } else {
-            num -= 1;  // -= 1 works much faster than --
-            counter++;
-        };
-    }
-    return counter;
+        (num % 2 === 0) ? num /= 2 : num--
+        count++;
+    };
+    return count;
 };
